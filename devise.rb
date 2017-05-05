2ee3ed26dc67b8de49975a9cdf2c49978f390cfc
alex duval <alexduval71@gmail.com>
2017-05-05 15:47:29 +0200

    version ON gem 'devise-bootstrap-views', '~> 0.0.11'

--- devise.rb
+++ devise.rb
@@ -22,7 +22,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-gem 'devise-bootstrap-views'
+gem 'devise-bootstrap-views', '~> 0.0.11'
 
 group :development, :test do
   gem 'binding_of_caller'
